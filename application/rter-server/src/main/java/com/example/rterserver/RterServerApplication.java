ff2b0c59f55cef7e465ed2b94fa67d5d37abfb8a
Alexandru-Paul Sîrbu <alexandru.sirbu2104@gmail.com>
2023-11-04 11:52:13 +0200

    RTER-19: added initial project setup (#1)
    Co-authored-by: Alexandru Sirbu <Alexandru.Sirbu@cloudflight.io>

---
+++ application/rter-server/src/main/java/com/example/rterserver/RterServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.rterserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RterServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RterServerApplication.class, args);
+    }
+
+}
