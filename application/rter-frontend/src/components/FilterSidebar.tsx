11e5830f2b4894000cc8f05c5d34d193d452519e
DragosMoro <morozan.dragos@gmail.com>
2023-11-15 00:23:12 +0200

    added fe with vite and added the structure

---
+++ application/rter-frontend/src/components/FilterSidebar.tsx
@@ -0,0 +1,5 @@
+const FilterSidebar = () => {
+  return <div>FilterSidebar</div>;
+};
+
+export default FilterSidebar;
---
+++ application/rter-frontend/src/components/Navbar.tsx
@@ -0,0 +1,7 @@
+const Navbar = () => {
+  return <div className="">
+    
+  </div>;
+};
+
+export default Navbar;
