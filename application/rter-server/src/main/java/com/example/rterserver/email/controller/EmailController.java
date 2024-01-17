a4f0c19f12aa294217bd2e50c1c5a7a77bd2dca7
bellaciaooo <salcedobellaa@gmail.com>
2024-01-17 15:00:11 +0200

    sending template email for donations(item,food) + solved bug for sending test emails for ngos

--- application/rter-server/src/main/java/com/example/rterserver/email/controller/EmailController.java
+++ application/rter-server/src/main/java/com/example/rterserver/email/controller/EmailController.java
@@ -1,5 +1,7 @@
 package com.example.rterserver.email.controller;
 
+import com.example.rterserver.donation.model.Donation;
+import com.example.rterserver.email.dto.DonationEmailRequest;
 import com.example.rterserver.email.dto.EmailRequest;
 import com.example.rterserver.email.service.EmailService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -17,6 +19,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmailController {
     @Autowired
     private EmailService emailService;
+
+    @Operation(summary = "Send a donation email for food or item", description = "This endpoint is used to send a donation email. ")
+    @PostMapping("/donation")
+    public ResponseEntity<?> sendDonationEmail(@RequestBody DonationEmailRequest donationEmailRequest) {
+        try {
+            emailService.sendDonationEmail(donationEmailRequest);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception ex) {
+            return new ResponseEntity<>("An error occurred while sending an email for the donation: " + ex.getMessage(),
+                    HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
 
     @Operation(summary = "Send an email", description = "This endpoint is used to send an email. ")
     @PostMapping
