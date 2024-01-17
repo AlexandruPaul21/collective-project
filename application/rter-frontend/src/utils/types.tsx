fa518c0bf366bb33620d43132dc4f963ed0a8109
Molnar Noemi Eveline <noemi.eveline.molnar@stud.ubbcluj.ro>
2024-01-17 16:39:28 +0200

    update NgoCard

--- application/rter-frontend/src/utils/types.tsx
+++ application/rter-frontend/src/utils/types.tsx
@@ -1,24 +1,32 @@
 export type CategoryItem = {
-    name: string;
-    items: string[];
-  };
-  
-  export type Categories = CategoryItem[];
+  name: string;
+  items: string[];
+};
 
-  export type User = {
-    id: string;
-    username: string;
-    name: string;
-    email: string;
-    password: string;
-    gender: Gender;
-    address: string;
-    description: string;
-  };
+export type Categories = CategoryItem[];
 
-  export type LoginRequest = {
-    username: string;
-    password: string;
-  }
+export type User = {
+  id: string;
+  username: string;
+  name: string;
+  email: string;
+  password: string;
+  gender: Gender;
+  address: string;
+  description: string;
+};
 
-  export enum Gender {MALE ="MALE" ,FEMALE="FEMALE"}
+export type LoginRequest = {
+  username: string;
+  password: string;
+}
+
+export enum Gender { MALE = "MALE", FEMALE = "FEMALE" }
+
+export enum COLORS {
+  DEEP_BLUE = "#1565C0",
+  TEAL = "#009688",
+  WHITE = "#FFFFFF",
+  RED = "#FF0000",
+  BLACK = "#000000",
+};
