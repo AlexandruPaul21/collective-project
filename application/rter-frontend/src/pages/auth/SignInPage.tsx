8d1c6be1eb1818f16574f58b3715aaf3fd751d1b
Deea <deea.lip@gmail.com>
2024-01-18 05:57:18 +0200

    finished sign-in resize (bug: doesnt work on physical phone)

--- application/rter-frontend/src/pages/auth/SignInPage.tsx
+++ application/rter-frontend/src/pages/auth/SignInPage.tsx
@@ -8,45 +8,19 @@ const SignInPage = () => {
   return (
     <div className="">
       {/* BIG SCREENS */}
-      {/*<div className="flex h-screen w-full">*/}
-      {/*  /!* IMAGE *!/*/}
-      {/*  <div className="flex-column flex w-3/5 items-center justify-center ">*/}
-      {/*    <img*/}
-      {/*      className="sm:p-[80px] lg:p-[100px]"*/}
-      {/*      src={logo}*/}
-      {/*      alt="login img"*/}
-      {/*    />*/}
-      {/*  </div>*/}
+      <div className="hidden lg:flex h-screen w-full">
+        {/* IMAGE */}
+        <div className="flex-column flex w-3/5 items-center justify-center ">
+          <img
+            className="sm:p-[80px] lg:p-[100px]"
+            src={logo}
+            alt="login img"
+          />
+        </div>
 
-      {/*  /!* SIGNIN FORM *!/*/}
-      {/*  <div*/}
-      {/*    className=" flex w-2/5 flex-col items-center justify-center rounded-l-[80px] border-b-4  border-l-4 border-t-4*/}
-      {/*   border-[#88C8FB] bg-[#F5F5F5] shadow-2xl"*/}
-      {/*  >*/}
-      {/*    <div className="w-1/2 scale-150 space-y-6">*/}
-      {/*      <div className="font-overpass text-darkgray  text-2xl font-extrabold tracking-wider">*/}
-      {/*        WELCOME BACK!*/}
-      {/*      </div>*/}
-      {/*      <div className="font-nunito text-darkgray  font] text-sm tracking-wider">*/}
-      {/*        Don't have an account,{" "}*/}
-      {/*        <Link*/}
-      {/*          to="/sign-up"*/}
-      {/*          className="text-lightblu font-semibold hover:text-[#2076C1]"*/}
-      {/*        >*/}
-      {/*          Sign up*/}
-      {/*        </Link>*/}
-      {/*      </div>*/}
-      {/*      <SignInForm/>*/}
-      {/*    </div>*/}
-      {/*    <Toaster/>*/}
-      {/*  </div>*/}
-      {/*</div>*/}
-
-      {/* SMALL SCREENS */}
-
-      <div className="flex h-screen justify-center">
+        {/* SIGNIN FORM */}
         <div
-          className=" flex small-screen-login-container  flex-col items-center justify-center rounded-[80px] border-4
+          className=" flex w-2/5 flex-col items-center justify-center rounded-l-[80px] border-b-4  border-l-4 border-t-4
          border-[#88C8FB] bg-[#F5F5F5] shadow-2xl"
         >
           <div className="w-1/2 scale-150 space-y-6">
@@ -66,7 +40,34 @@ const SignInPage = () => {
           </div>
           <Toaster/>
         </div>
+      </div>
 
+      {/* SMALL SCREENS */}
+
+      <div className="lg:hidden flex items-center justify-center">
+      <div className="flex h-[600px]  justify-center">
+        <div
+          className=" flex small-screen-login-container  flex-col items-center justify-center rounded-[80px] border-4
+         border-[#88C8FB] bg-[#F5F5F5] shadow-2xl"
+        >
+          <div className="w-1/2 scale-125 space-y-6">
+            <div className="font-overpass text-darkgray  text-2xl font-extrabold tracking-wider">
+              WELCOME BACK!
+            </div>
+            <div className="font-nunito text-darkgray  font] text-sm tracking-wider">
+              Don't have an account,{" "}
+              <Link
+                to="/sign-up"
+                className="text-lightblu font-semibold hover:text-[#2076C1]"
+              >
+                Sign up
+              </Link>
+            </div>
+            <SignInForm/>
+          </div>
+          <Toaster/>
+        </div>
+      </div>
       </div>
     </div>
   );
